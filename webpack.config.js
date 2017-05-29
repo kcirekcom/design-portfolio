77be070473e34744e71f4fa0c6c6c90f7dfca825
kcirekcom <erick.f.mock@gmail.com>
2017-05-28 23:28:38 -0600

    setting up

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.jsx',
+  output: {
+    path: __dirname,
+    filename: 'bundle.index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(woff|ttf|svg|eot).*/,
+        loader: 'url?limit=10000&name=font/[hash].[ext]'
+      },
+      {
+        test: /\.(jpg|jpeg|svg|bmp|tiff|gif|png)$/,
+        loader: 'url?limit=10000&name=image/[hash].[ext]'
+      }
+    ]
+  }
+};
