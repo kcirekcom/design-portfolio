27658576f0e8d65c14fed96e6e36fc759a28e554
kcirekcom <erick.f.mock@gmail.com>
2017-06-02 13:02:42 -0600

    removed lines from index jsx///changed web icon

--- app/index.jsx
+++ app/index.jsx
@@ -11,19 +11,11 @@ import { Router, Route, hashHistory } from 'react-router';
 require('./scss/main.scss');
 
 ReactDOM.render(
-    <div>
-        <Router history={hashHistory}>
-            <Route path='/' component={HomePage}/>
-            <Route path='/visualizations' component={Visualization}/>
-            <Route path='/prototypes' component={Prototype}/>
-            <Route path='/development' component={Development}/>
-        </Router>
-        {/*<div className='vertical-line-left'></div>
-        <div className='vertical-line-right'></div>
-        <div className='horizontal-line-left'></div>
-        <div className='horizontal-line-right'></div>
-        <div className='diagonal-line-left'></div>
-        <div className='diagonal-line-right'></div>*/}
-    </div>,
+    <Router history={hashHistory}>
+        <Route path='/' component={HomePage}/>
+        <Route path='/visualizations' component={Visualization}/>
+        <Route path='/prototypes' component={Prototype}/>
+        <Route path='/development' component={Development}/>
+    </Router>,
     document.getElementById('react-app')
 );
